acb4590da20efd79a41b9016bbee6102d295e243
Micael Jerry <hei.jerry.2@gmail.com>
2023-12-06 17:29:43 +0300

    fix: empty history

--- src/view/HistoryView.java
+++ src/view/HistoryView.java
@@ -7,6 +7,10 @@ import java.util.List;
 public class HistoryView {
     public static void printHistory(List<History> historyList) {
         System.out.println("===================== History =====================");
+        if (historyList.isEmpty()) {
+            System.out.println("Empty history");
+            return;
+        }
         for (History history : historyList) {
             System.out.println(history.getTimestamp().toString() + " - " + history.getOperation() + " - " + "sold: " + history.getWallet().getSold());
         }
