16100d4cf07824732f2b846e7615b295dcf82eac
Micael Jerry <hei.jerry.2@gmail.com>
2023-12-09 11:51:59 +0300

    fix: remove note

--- src/app/WalletApp.java
+++ src/app/WalletApp.java
@@ -1,26 +1,22 @@
 package app;
 
-import model.Note;
 import model.Wallet;
 import view.BarView;
 import view.MenuView;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class WalletApp {
     public static void run () {
         Scanner sc = new Scanner(System.in);
         Wallet wallet = new Wallet();
-        List<Note> noteList = new ArrayList<>();
 
         System.out.println("Welcome");
 
         boolean conditionRunning = true;
         while (conditionRunning) {
             BarView.bar("Menu");
-            MenuView.printMenu("Money", "Note", "Exit");
+            MenuView.printMenu("Money", "Exit");
             while (!sc.hasNextInt()) {
                 System.out.println("Please enter a valid number");
                 sc.next();
@@ -31,9 +27,6 @@ public class WalletApp {
                     MoneyApp.run(wallet.getMoney(), sc);
                     break;
                 case 2:
-                    NoteApp.run(noteList, sc);
-                    break;
-                case 3:
                     conditionRunning = false;
                     System.out.println("Bye");
                     sc.close();
